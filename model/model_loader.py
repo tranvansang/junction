6614dac48747c97b4efc0f518e27ff63b19c82b9
TianrenWang <broodstar_frank@hotmail.com>
2019-02-16 15:03:25 +0900

    completed training data parsing. upgraded toy model

---
+++ model/model_loader.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+import sys
+
+def init():
+    tf.reset_default_graph()
+    init = tf.global_variables_initializer()
+    session = tf.Session()
+    #the next few lines load the model
+    #i want to input obj here instead of 'model.meta' string
+    saver = tf.train.import_meta_graph('model.meta')
+    saver.restore(session,tf.train.latest_checkpoint('./'))
+    session.run(init)
+    
+    return session
+
+def chat(session, inputValue, currentState):
+	graph = tf.get_default_graph()
+	X = graph.get_tensor_by_name("X:0")
+	previous_state = graph.get_tensor_by_name("prev_state:0")
+	multiply_op = graph.get_tensor_by_name("multiply_op:0")
+	state_op = graph.get_tensor_by_name("state_op:0")
+	output, newState = session.run([multiply_op, state_op], feed_dict={X: inputValue, previous_state: currentState})
+	return newState, output
+
+def main():
+    session = init()
+    newState = np.array([2]).astype(np.float32)
+    while True:
+        inputValue = np.array([int(input())])
+        newState, output = chat(session, inputValue, newState)
+        print("output: " + str(output))
+        print("newState: " + str(newState))
+
+main()
